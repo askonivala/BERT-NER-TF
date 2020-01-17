3bc56540abc8855323e632f038dc610ae4a61e04
Asko Nivala <aeniva@utu.fi>
2020-01-17 11:39:47 +0100

    Implement sentence tokenization to API to avoid too long buffer error.

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 from bert import Ner
 
+import nltk.data
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
 app = Flask(__name__)
 CORS(app)
 
@@ -11,9 +14,15 @@ model = Ner("out_base")
 @app.route("/predict",methods=['POST'])
 def predict():
     text = request.json["text"]
+    sentences = sent_detector.tokenize(text)
+
     try:
-        out = model.predict(text)
-        return jsonify({"result":out})
+        ner_tagged_sentences = []
+        for sentence in sentences:
+            tagged_sentence = model.predict(sentence)
+            ner_tagged_sentences.extend(tagged_sentence)
+        # out = ''.join(ner_tagged_sentences)
+        return jsonify({"result":ner_tagged_sentences})
     except Exception as e:
         print(e)
         return jsonify({"result":"Model Failed"})
